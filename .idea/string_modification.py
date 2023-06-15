939ce92c5b4615b3a8fc25688d5113974cde6aee
Zoe Smith <107585629+ZoeGrey86@users.noreply.github.com>
2023-06-15 12:05:54 -0500

    code clean up

--- .idea/string_modification.py
+++ .idea/string_modification.py
@@ -1,10 +1,7 @@
 def pig_latin(string):
     pig = string[2:len(string)] + string[0] + string[1] + 'a'
-    for x in range (len(string)):
-        x = x + 1
     print(pig)
 
 def run_program():
-    print('please input a string')
-    string = input('')
+    string = input('please input a string\n')
     pig_latin(string)
